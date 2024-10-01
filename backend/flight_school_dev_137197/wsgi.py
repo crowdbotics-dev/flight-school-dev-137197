c193742236559c1ad9d4a783645ddb0675de34ac
Crowdbotics <engineering@crowdbotics.com>
2024-10-01 21:50:39 +0000

    Initial commit

---
+++ backend/flight_school_dev_137197/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for flight_school_dev_137197 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "flight_school_dev_137197.settings"
+)
+
+application = get_wsgi_application()
